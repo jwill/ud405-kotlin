050139026fddaef7362160e2c8d70e4c783317ee
James Williams <James.L.Williams@gmail.com>
2015-12-25 22:48:25 -0500

    Added Solar System demo.

---
+++ 1.4.04-Demo-SolarSystem/desktop/src/com/udacity/gamedev/closeup/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.udacity.gamedev.closeup.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.udacity.gamedev.closeup.Closeup;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Closeup(), config);
+	}
+}
